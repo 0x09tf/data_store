4de4c45feea0a167d9a02d4329a39e3e28964932
0x09tf <62036423+0x09tf@users.noreply.github.com>
2020-04-04 15:38:06 -0400

    Create core_dumps.js

---
+++ core_dumps.js
@@ -0,0 +1,73 @@
+{
+  "result": "success",
+  "files": [
+    {
+      "path": "core_dumps/morphic",
+      "is_directory": true,
+      "updated_at": "Thu, 02 Apr 2020 23:39:26 -0000"
+    },
+    {
+      "path": "core_dumps/babylon.html",
+      "is_directory": false,
+      "size": 13535,
+      "updated_at": "Thu, 02 Apr 2020 19:45:29 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/birth_chart.html",
+      "is_directory": false,
+      "size": 3059,
+      "updated_at": "Fri, 27 Mar 2020 03:33:13 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/concept_maps.html",
+      "is_directory": false,
+      "size": 6044,
+      "updated_at": "Sat, 04 Apr 2020 03:37:37 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/index.html",
+      "is_directory": false,
+      "size": 2629,
+      "updated_at": "Thu, 02 Apr 2020 19:44:33 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/jquery_app.html",
+      "is_directory": false,
+      "size": 5600,
+      "updated_at": "Tue, 31 Mar 2020 22:46:21 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/misfits.jpg",
+      "is_directory": false,
+      "size": 129910,
+      "updated_at": "Thu, 26 Mar 2020 01:30:09 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/phaser3.html",
+      "is_directory": false,
+      "size": 4216,
+      "updated_at": "Sat, 04 Apr 2020 03:17:02 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/social_net.html",
+      "is_directory": false,
+      "size": 10245,
+      "updated_at": "Sun, 29 Mar 2020 21:09:18 -0000",
+      "sha1_hash": null
+    },
+    {
+      "path": "core_dumps/timeline.html",
+      "is_directory": false,
+      "size": 8189,
+      "updated_at": "Mon, 30 Mar 2020 03:44:28 -0000",
+      "sha1_hash": null
+    }
+  ]
+}
